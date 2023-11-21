2f91c84ca75125ef8e9044428a1a3045afe5a74c
ljw03079 <ljw03079@naver.com>
2023-11-22 00:18:55 +0900

    zz

---
+++ project/src/main/java/com/tenniswing/project/court/web/CourtController.java
@@ -0,0 +1,13 @@
+package com.tenniswing.project.court.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CourtController {
+	@GetMapping("court")  
+	public String courtPage(Model model) { 			
+		return "court/court";
+	}
+}
