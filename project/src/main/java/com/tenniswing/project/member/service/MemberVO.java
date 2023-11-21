c3298d47f066f791fa7956f8fc9e778c3518c6ae
inspireman-jang <87566485+inspireman-jang@users.noreply.github.com>
2023-11-22 00:21:00 +0900

    20231122 member

--- project/src/main/java/com/tenniswing/project/member/service/MemberVO.java
+++ project/src/main/java/com/tenniswing/project/member/service/MemberVO.java
@@ -1,5 +1,26 @@
 package com.tenniswing.project.member.service;
 
-public class MemberVO {
+import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+	// 1) 컬럼따라 타입을 구분 or 실제 타입 대신 String
+	// 2) 기본타입(int, double 등) or Warp 클래스(Integer, Double)
+	private int employeeId; /* private Integer employeedId; */
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phoneNumber;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date hireDate;
+	private String jobId;
+	private double salary;
+	private Double commissionPct;
+	private Integer managerId;
+	private Integer departmentId;
 }
+
