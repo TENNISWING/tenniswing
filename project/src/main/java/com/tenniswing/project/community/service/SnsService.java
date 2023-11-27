c2cd6410908c21146475ff68108f2009d6be885c
jeongjaeryun <innisee@naver.com>
2023-11-28 00:36:52 +0900

    select 매퍼

---
+++ project/src/main/java/com/tenniswing/project/community/service/SnsService.java
@@ -0,0 +1,34 @@
+package com.tenniswing.project.community.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.data.repository.query.Param;
+
+public interface SnsService {
+	// 전체조회
+	public List<SnsVO> selectAllSnsInfo();
+	
+	// 단건조회
+	public SnsVO selectSnsInfo(SnsVO snsVO);
+	
+	// 등록
+		public int insertSns(SnsVO snsVO);
+		// 그룹등록
+		public int insertSnsGrp(SnsVO snsVO);
+		// 좋아요 등록
+		public int insertSnsLike(SnsVO snsVO);
+		// 스크랩 등록
+		public int insertSnsScrap(SnsVO snsVO);
+		// 댓글 등록
+		public int insertSnsRep(SnsVO snsVO);
+		// 대댓글 등록
+		public int insertSnsRrep(SnsVO snsVO);
+		
+	// 수정
+	public Map<String, Object> updateSns(SnsVO snsVO);
+		
+	// 삭제
+	public int deleteSns(@Param("snsWrtNo")int snsWrtNo );
+
+}
