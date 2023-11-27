f28cec74e418a946781a3b780500f70e5d5a80b4
lee2jin <maa01@naver.com>
2023-11-27 11:15:22 +0900

    jin 1127 점심커밋

--- project/src/main/java/com/tenniswing/project/club/web/ClubController.java
+++ project/src/main/java/com/tenniswing/project/club/web/ClubController.java
@@ -17,9 +17,45 @@ public class ClubController {
 		return "club/clubform";
 	}
 	
-	@GetMapping("clubdetail")  
+	@GetMapping("clubdetail")  //상세페이지
 	public String clubDetailPage(Model model) { 		
 		return "club/clubdetail";
 	}
+	
+	@GetMapping("clubInfo")  //상세페이지 > 탭 > 클럽정보
+	public String infoTapPage(Model model) { 		
+		return "club/clubInfo";
+	}
+	
+	@GetMapping("clubMatchJoin")  //상세페이지 > 탭 > 매치모집
+	public String joinTapPage(Model model) { 		
+		return "club/clubMatchJoin";
+	}
+	
+	@GetMapping("clubMatchDate")  //상세페이지 > 탭 > 매치일정
+	public String DateTapPage(Model model) { 		
+		return "club/clubMatchDate";
+	}
+	
+	@GetMapping("clubMatchEnd")  //상세페이지 > 탭 > 매치결과
+	public String EndTapPage(Model model) { 		
+		return "club/clubMatchEnd";
+	}
+	
+	@GetMapping("clubBoard")  //상세페이지 > 탭 > 자유게시판
+	public String boardTapPage(Model model) { 		
+		return "club/clubBoard";
+	}
+	
+	@GetMapping("clubMember")  //상세페이지 > 탭 > 멤버
+	public String memberTapPage(Model model) { 		
+		return "club/clubMember";
+	}
+	
+	@GetMapping("clubApply")  //상세페이지 > 탭 > 클럽가입신청
+	public String applyTapPage(Model model) { 		
+		return "club/clubApply";
+	}
+	
 	
 }
